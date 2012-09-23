01573831e1ec1a8dcaab6cc44215667ad903c14a
Alexx Roche <notice-dev@alexx.net>
2012-09-23 12:49:43 +0200

    webSockets Hybi-17 working
    tested in Google Chrome 18.0.1025.162 and Firefox 15.0.1

---
+++ scripts/php/webSockets/phpwebsocket/websocket.demo.php
@@ -0,0 +1,8 @@
+#!/usr/bin/php -q
+<?php  
+	// Run from command prompt > php -q websocket.demo.php
+
+	// Basic WebSocket demo echoes msg back to client
+	include "websocket.class.php";
+	$master = new WebSocket ("localhost", 12345);
+?>
